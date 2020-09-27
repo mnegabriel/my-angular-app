3d135fc89326e3f64a6a2a8d76b9b5f763eba428
Gabriel Dantas <gabrielmd123@gmail.com>
2020-09-26 23:08:56 -0300

    tutorial 5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs'
-import { MessageService } from './message.service'
 
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
+import { MessageService } from './message.service'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -16,5 +15,11 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: Fetched heroes')
     return of(HEROES)
+  }
+
+  getHero(id: number): Observable<Hero> {
+
+    this.messageService.add(`HeroService: Fetched hero with id = ${id}`)
+    return of(HEROES.find( hero => hero.id === id))
   }
 }
